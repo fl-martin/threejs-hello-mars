043cd581d47c00d9890b6e9ab38c629397d2a016
Federico L. Martin <fl.martin.dev@gmail.com>
2021-12-04 17:46:19 -0300

    Changing Imagenes folder name to Images

--- src/Textures/MetalMesh.js
+++ src/Textures/MetalMesh.js
@@ -1,11 +1,11 @@
 import TexturesLoader from "./TexturesLoader";
-import basecolor from "../Imagenes/Metal_Mesh_008_SD/basecolor.jpg";
-import normalmap from "../Imagenes/Metal_Mesh_008_SD/normalmap.jpg";
-import displacementmap from "../Imagenes/Metal_Mesh_008_SD/heightmap.png";
-import roughnessmap from "../Imagenes/Metal_Mesh_008_SD/roughnessmap.jpg";
-import aomap from "../Imagenes/Metal_Mesh_008_SD/aomap.jpg";
-import alphamap from "../Imagenes/Metal_Mesh_008_SD/opacitymap.jpg";
-import metallicmap from "../Imagenes/Metal_Mesh_008_SD/metallicmap.jpg";
+import basecolor from "../Images/Metal_Mesh_008_SD/basecolor.jpg";
+import normalmap from "../Images/Metal_Mesh_008_SD/normalmap.jpg";
+import displacementmap from "../Images/Metal_Mesh_008_SD/heightmap.png";
+import roughnessmap from "../Images/Metal_Mesh_008_SD/roughnessmap.jpg";
+import aomap from "../Images/Metal_Mesh_008_SD/aomap.jpg";
+import alphamap from "../Images/Metal_Mesh_008_SD/opacitymap.jpg";
+import metallicmap from "../Images/Metal_Mesh_008_SD/metallicmap.jpg";
 
 export default function MetalMesh() {
 	const baseColor = TexturesLoader.load(basecolor);
--- src/Textures/SurfaceImperfection.js
+++ src/Textures/SurfaceImperfection.js
@@ -1,9 +1,9 @@
 import TexturesLoader from "./TexturesLoader";
-import basecolor from "../Imagenes/SurfaceImperfections/basecolor.jpg";
-import normalmap from "../Imagenes/SurfaceImperfections/normalmap.jpg";
-import displacementmap from "../Imagenes/SurfaceImperfections/heightmap.png";
-import roughnessmap from "../Imagenes/SurfaceImperfections/roughnessmap.jpg";
-import aomap from "../Imagenes/SurfaceImperfections/roughnessmap.jpg";
+import basecolor from "../Images/SurfaceImperfections/basecolor.jpg";
+import normalmap from "../Images/SurfaceImperfections/normalmap.jpg";
+import displacementmap from "../Images/SurfaceImperfections/heightmap.png";
+import roughnessmap from "../Images/SurfaceImperfections/roughnessmap.jpg";
+import aomap from "../Images/SurfaceImperfections/roughnessmap.jpg";
 
 export default function SurfaceImperfection() {
 	const baseColor = TexturesLoader.load(basecolor);
